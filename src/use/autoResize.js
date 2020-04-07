9fc43212e6700120b5040ec37892a1beab315fd1
yizhiyuyou <949267840@qq.com>
2020-04-07 12:20:27 +0800

    优化 autoResize 代码

--- src/use/autoResize.js
+++ src/use/autoResize.js
@@ -1,47 +1,30 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { debounce, observerDomResize } from '../util/index'
 
 export default function useAutoResize() {
   const [state, setState] = useState({ width: 0, height: 0 })
 
   const domRef = useRef(null)
-  const domObserverRef = useRef(null)
-  const debounceSetWHFunRef = useRef(null)
-
-  const setWH = useCallback(() => {
-    const { clientWidth, clientHeight } = domRef.current
-
-    setState({ width: clientWidth, height: clientHeight })
-  }, [])
-
-  const bindDomResizeCallback = useCallback(() => {
-    domObserverRef.current = observerDomResize(
-      domRef.current,
-      debounceSetWHFunRef.current
-    )
-
-    window.addEventListener('resize', debounceSetWHFunRef.current)
-  }, [])
-
-  const unbindDomResizeCallback = useCallback(() => {
-    const { current: domObserver } = domObserverRef
-
-    domObserver.disconnect()
-    domObserver.takeRecords()
-    domObserverRef.current = null
-
-    window.removeEventListener('resize', debounceSetWHFunRef.current)
-  }, [])
 
   useEffect(() => {
-    debounceSetWHFunRef.current = debounce(setWH, 100)
+    const debounceSetWHFun = debounce(() => {
+      const { clientWidth, clientHeight } = domRef.current
 
-    debounceSetWHFunRef.current()
+      setState({ width: clientWidth, height: clientHeight })
+    }, 100)
 
-    bindDomResizeCallback()
+    debounceSetWHFun()
 
-    // 组件销毁时，清除事件
-    return unbindDomResizeCallback
+    const domObserver = observerDomResize(domRef.current, debounceSetWHFun)
+
+    window.addEventListener('resize', debounceSetWHFun)
+
+    return () => {
+      domObserver.disconnect()
+      domObserver.takeRecords()
+  
+      window.removeEventListener('resize', debounceSetWHFun)
+    }
   }, [])
 
   return { ...state, domRef }
